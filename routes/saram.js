67ba77d232f861cad33b32c9432848e9f3dc560b
Ahn-Gihwan <ahngh0113@naver.com>
2022-02-21 18:12:02 +0900

    feat: get list by body

--- routes/saram.js
+++ routes/saram.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const axios = require('axios')
+require('dotenv').config();
 const headers = {
   Accept:'application/json'
 }
 
-router.route('/').get((req,res)=>{
+const URL = `https://oapi.saramin.co.kr/job-search?access-key=${process.env.API_KEY}`
+
+router.route('/').post((req,res)=>{
+  const {keywords, loc_cd} = req.body
+  
+  const url = URL + (keywords ? `&keywords=${encodeURI(keywords)}`:'') + (loc_cd ? `&loc_cd=${loc_cd}`:'')
+
   axios({
     method : 'GET',
-    url : `https://oapi.saramin.co.kr/job-search?access-key=${process.env.API_KEY}`,
+    url,
     headers
   }).then(response => res.json(response.data))
 })
 
 router.route('/:id').get((req,res)=>{
   const { id } = req.params
+  const url = URL + `&id=${id}`
   
   axios({
     method : 'GET',
-    url : `https://oapi.saramin.co.kr/job-search?access-key=${process.env.API_KEY}&id=${id}`,
+    url ,
     headers
   }).then(response => res.json(response.data))
 })

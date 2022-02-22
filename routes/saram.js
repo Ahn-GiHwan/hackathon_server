7b2d5d5565e1e4adea1954ecb4db3b81318b77bb
Ahn-Gihwan <ahngh0113@naver.com>
2022-02-22 19:07:51 +0900

    feat: saram add job

--- routes/saram.js
+++ routes/saram.js
@@ -5,12 +5,36 @@ require("dotenv").config();
 const headers = {
   Accept: "application/json",
 };
+const it = require("../dataTable/IT");
 
 const URL = `https://oapi.saramin.co.kr/job-search?access-key=${process.env.API_KEY}`;
 
 router.route("/:id").get((req, res) => {
   const { id } = req.params;
   const url = URL + `&id=${id}`;
+
+  axios({
+    method: "GET",
+    url,
+    headers,
+  }).then((response) => res.json(response.data));
+});
+
+router.route("/job/:job").get((req, res) => {
+  const { job } = req.params;
+  let code = null;
+
+  it.forEach((el) => {
+    if (el.name === job) {
+      code = el.code;
+    }
+  });
+
+  if (!code) {
+    res.status(404).send("존재하지 않는 직무입니다.");
+  }
+
+  const url = URL + `&job_cd=${code}`;
 
   axios({
     method: "GET",
